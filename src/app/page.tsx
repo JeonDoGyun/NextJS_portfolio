c655ce1095631103fb8330cf9f67458cd2cf9d16
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-29 21:47:35 +0900

    feat: IntroSection에서 일부 내용 분리
    InfoSection 파트를 따로 컴포넌트로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import InfoSection from "./sections/InfoSection";
 import IntroSection from "./sections/IntroSection";
 import StrengthSection from "./sections/StrengthSection";
 
 const Home = () => {
   return (
     <main className="flex flex-col justify-center items-center">
+      <InfoSection />
       <IntroSection />
       <StrengthSection />
     </main>

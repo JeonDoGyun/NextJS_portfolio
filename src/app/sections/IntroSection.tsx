748c1f6cc78d693afc5ed742ab53b2cc79d6f17d
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-29 21:45:56 +0900

    style: 개발자 특징 소개 카드 layout 변경

--- src/app/sections/IntroSection.tsx
+++ src/app/sections/IntroSection.tsx
@@ -4,28 +4,37 @@ import Banner from "../components/Banner";
 
 const IntroSection = () => {
   return (
-    <section className="h-screen rounded-3xl text-white">
-      <article className="introduction">
-        <Banner textSize="text-4xl">안녕하세요!</Banner>
-        <Banner textSize="text-2xl">
-          Frontend 개발자 전도균의 포트폴리오입니다
-        </Banner>
-      </article>
-      <article className="info">
-        <Banner textSize="text-4xl">Info</Banner>
-      </article>
-      <article className="about-me">
-        <div className="grid grid-cols-3 mx-4">
-          <div className="text-4xl col-span-1 flex flex-col text-center">
-            <Banner textSize="text-4xl">About</Banner>
-            <Banner textSize="text-4xl">Me</Banner>
-          </div>
-          <div className="text-area col-span-2">
+    <section className="body-font h-screen text-white">
+      <div className="container px-5 py-24 mx-auto">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-20">
+          <Banner textSize="text-4xl">About Me</Banner>
+          <br className="hidden sm:block" />
+          <div className="text-cneter">
             <Banner textSize="text-3xl">
               이러한 개발자가 되기 위해 노력하고 있습니다
             </Banner>
-            <div className="border-4 border-gray-400 rounded-md px-4 py-4 my-2">
-              <Banner textSize="text-xl">노력하는 개발자</Banner>
+          </div>
+        </h1>
+        <div className="flex flex-row sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 w-auto gap-2">
+          <div className="p-2 py-4 md:w-1/3 flex border-2 border-yellow-500 shadow-md shadow-yellow-500 rounded-md">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-lg title-font font-medium mb-2">
+                노력하는 개발자
+              </h2>
               <ul className="list-disc">
                 <li className="mx-2">
                   “우물 안 개구리가 되지 말자” 라는 생각으로 새로운 지식을 얻게
@@ -38,8 +47,26 @@ const IntroSection = () => {
                 </li>
               </ul>
             </div>
-            <div className="border-4 border-gray-400 rounded-md p-4 my-2">
-              <Banner textSize="text-xl">질문하는 개발자</Banner>
+          </div>
+          <div className="p-2 py-4 md:w-1/3 flex border-2 border-yellow-500 shadow-md shadow-yellow-500 rounded-md">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-lg title-font font-medium mb-2">
+                질문하는 개발자
+              </h2>
               <ul className="list-disc">
                 <li className="mx-2">
                   문제에 부딪혔을 때 혼자서만 해결책을 찾아 가기보단, 주변에
@@ -51,8 +78,26 @@ const IntroSection = () => {
                 </li>
               </ul>
             </div>
-            <div className="border-4 border-gray-400 rounded-md p-4 my-2">
-              <Banner textSize="text-xl">소통하는 개발자</Banner>
+          </div>
+          <div className="p-2 py-4 md:w-1/3 flex border-2 border-yellow-500 shadow-md shadow-yellow-500 rounded-md">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-lg title-font font-medium mb-2">
+                소통하는 개발자
+              </h2>
               <ul className="list-disc">
                 <li className="mx-2">
                   혼자만의 개발보단 서로의 지식을 공유하는 자리에 참석하는 것을
@@ -66,7 +111,7 @@ const IntroSection = () => {
             </div>
           </div>
         </div>
-      </article>
+      </div>
     </section>
   );
 };
